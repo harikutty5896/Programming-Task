56583bcd9a4bce97d77b1da9662779290ca850ad
Hari <Harie20202@gmail.com>
2022-07-12 22:07:05 +0530

    Revert "Singleton Thread safe example"
    This reverts commit 3e01485053ba9c8a0f4fd7e07b113ea52c739e87.

--- Singleton.cpp
+++ Singleton.cpp
@@ -1,67 +1,98 @@
-#include "pch.h"
-#include <iostream>
-#include <mutex>  
-#include <chrono>
-#include <thread>
-#include <string>
-using namespace std;
 class Singleton
 {
+
+    /**
+     * The Singleton's constructor/destructor should always be private to
+     * prevent direct construction/desctruction calls with the `new`/`delete`
+     * operator.
+     */
 private:
-    static mutex mtx;
-    static Singleton* instance;
+    static Singleton * pinstance_;
+    static std::mutex mutex_;
 
 protected:
-    string value;
-    Singleton(string v) :value(v)
+    Singleton(const std::string value): value_(value)
     {
     }
+    ~Singleton() {}
+    std::string value_;
 
 public:
-    Singleton(Singleton& other) = delete;
-    void operator=(const Singleton&) = delete;
-    static Singleton* GetInstance(const string& value);
-    string getValue() const
+    /**
+     * Singletons should not be cloneable.
+     */
+    Singleton(Singleton &other) = delete;
+    /**
+     * Singletons should not be assignable.
+     */
+    void operator=(const Singleton &) = delete;
+    /**
+     * This is the static method that controls the access to the singleton
+     * instance. On the first run, it creates a singleton object and places it
+     * into the static field. On subsequent runs, it returns the client existing
+     * object stored in the static field.
+     */
+
+    static Singleton *GetInstance(const std::string& value);
+    /**
+     * Finally, any singleton should define some business logic, which can be
+     * executed on its instance.
+     */
+    void SomeBusinessLogic()
     {
-        return value;
+        // ...
     }
+    
+    std::string value() const{
+        return value_;
+    } 
 };
 
-Singleton* Singleton::instance{ nullptr };
-mutex Singleton::mtx;
+/**
+ * Static methods should be defined outside the class.
+ */
 
-Singleton* Singleton::GetInstance(const string& value)
+Singleton* Singleton::pinstance_{nullptr};
+std::mutex Singleton::mutex_;
+
+/**
+ * The first time we call GetInstance we will lock the storage location
+ *      and then we make sure again that the variable is null and then we
+ *      set the value. RU:
+ */
+Singleton *Singleton::GetInstance(const std::string& value)
 {
-    lock_guard<mutex> lock(mtx);
-    if (instance == nullptr)
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (pinstance_ == nullptr)
     {
-        instance = new Singleton(value);
+        pinstance_ = new Singleton(value);
     }
-    return instance;
+    return pinstance_;
 }
 
-void Threadone()
-{
-    this_thread::sleep_for(chrono::milliseconds(1000));
-    Singleton* singleton = Singleton::GetInstance("one");
-    cout << singleton->getValue() << endl;
-
+void ThreadFoo(){
+    // Following code emulates slow initialization.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Singleton* singleton = Singleton::GetInstance("FOO");
+    std::cout << singleton->value() << "\n";
 }
 
-void Threadtwo()
-{
-    this_thread::sleep_for(chrono::milliseconds(1000));
-    Singleton* singleton = Singleton::GetInstance("two");
-    cout << singleton->getValue() << endl;
+void ThreadBar(){
+    // Following code emulates slow initialization.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Singleton* singleton = Singleton::GetInstance("BAR");
+    std::cout << singleton->value() << "\n";
 }
 
 int main()
-{
-    thread t1(Threadone);
-    thread t2(Threadtwo);
+{   
+    std::cout <<"If you see the same value, then singleton was reused (yay!\n" <<
+                "If you see different values, then 2 singletons were created (booo!!)\n\n" <<
+                "RESULT:\n";   
+    std::thread t1(ThreadFoo);
+    std::thread t2(ThreadBar);
     t1.join();
     t2.join();
+    
     return 0;
 }
-
-
